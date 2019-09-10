be7aaee7635c5ac6242720da4a67223ff1fc8c72
Aditya <sp.aditya@gmail.com>
2019-09-10 22:04:42 +0530

    Add files via upload

---
+++ progs/adder.py
@@ -0,0 +1,14 @@
+ui = input("Enter , seperated string of integers:" ).strip().split(',')
+print("Summation is:", sum(map(int, ui)))
+
+
+
+
+
+
+
+#1,2,3 ---> ['1', '2', '3']
+#total = 0
+#for i in ui:
+#    total += int(i)
+#print("The summation is", total)
