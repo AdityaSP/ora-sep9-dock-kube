87a968b3e1082103cd8b05336a08f09b8af7af8f
Aditya <sp.aditya@gmail.com>
2019-09-11 22:28:56 +0530

    Add files via upload

--- progs/posterdownload.py
+++ progs/posterdownload.py
@@ -1,8 +1,24 @@
 import requests
 import json
+import os
+
 moviename = input("Enter a movie name: ")
+if os.path.exists(moviename):
+    res= input("Already exists.Do you want to overwrite?: ")
+    if res.strip().upper() not in ['Y', 'YES','1']:
+        print("Exiting..")
+        exit(-1)
+else:        
+    os.mkdir(moviename)
 
 r = requests.get(r'http://www.omdbapi.com/?s='+ moviename +'&apikey=b4e17ea0')
+
+def download(posterurl, loc):
+    poster = requests.get(posterurl)
+    if poster.ok:
+        fh = open( loc ,'wb')
+        fh.write(poster.content)
+        fh.close()
 
 if r.ok:
     data = json.loads(r.text)
@@ -10,8 +26,4 @@ if r.ok:
         print("Downloading poster for ", movie['Title'])
         posterurl = movie['Poster']
         if posterurl != 'N/A':
-            poster = requests.get(posterurl)
-            if poster.ok:
-                fh = open( movie['imdbID'] + ".jpg" ,'wb')
-                fh.write(poster.content)
-                fh.close()
+            download(posterurl, moviename + os.path.sep +movie['imdbID'] + ".jpg")
