4e87e931c132b777cc14f8fc45db509282aedfdd
Aditya <sp.aditya@gmail.com>
2019-09-12 02:15:59 +0530

    Add files via upload

--- module/bank.py
+++ module/bank.py
@@ -1,5 +1,5 @@
 class Account():
-    def __init__(self, n, b=1000, t='S'):
+    def __init__(self, n, b, t):
         self.n = n
         self.b = b
         self.t = t
@@ -7,5 +7,19 @@ class Account():
         self.b += amount
     def debit(self, amount):
         self.b -= amount
+
+class SA(Account):
+    def __init__(self, n, b=1000):
+        Account.__init__(self, n, b, 'S')
+    def debit(self, amount):
+        # TODO- convert to exceptions
+        if self.b < amount:
+            print("Insufficient Balance")
+        else:
+            Account.debit(self, amount)
+
+class CA(Account):
+    def __init__(self, n, b=0):
+        Account.__init__(self, n, b, 'C')
         
         
