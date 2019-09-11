9ebfd865237f7c084492701b60c93da4b40d845b
Aditya <sp.aditya@gmail.com>
2019-09-12 02:15:33 +0530

    Add files via upload

--- progs/transcations.py
+++ progs/transcations.py
@@ -1,7 +1,24 @@
 import bank
-ac1 = bank.Account('Aditya')
-print(ac1.n, ac1.b)
-ac1.debit(100)
-print(ac1.n, ac1.b)
-ac1.credit(1000000)
-print(ac1.n, ac1.b)
+sa = bank.SA('Aditya')
+print(sa.n, sa.b)
+sa.credit(100)
+sa.debit(2000)
+print(sa.n, sa.b)
+
+ca = bank.CA('ABC Inc.')
+print(ca.n, ca.b)
+ca.credit(100)
+ca.debit(2000)
+print(ca.n, ca.b)
+
+
+
+
+
+
+#ac1 = bank.Account('Aditya')
+#print(ac1.n, ac1.b)
+#ac1.debit(100)
+#print(ac1.n, ac1.b)
+#ac1.credit(1000000)
+#print(ac1.n, ac1.b)
