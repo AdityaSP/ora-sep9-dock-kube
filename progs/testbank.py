fbb00e1f7ea89b45d52c988878d6d4c643dfdacc
Aditya <sp.aditya@gmail.com>
2019-09-12 22:01:40 +0530

    Add files via upload

---
+++ progs/testbank.py
@@ -0,0 +1,20 @@
+import unittest
+import bank
+class TestBank(unittest.TestCase):
+    def test_sa_create(self):
+        sa = bank.SA('Aditya')
+        assert sa.b == 1000, "US101: default bal should be 1000"
+    def test_ca_create(self):
+        ca = bank.CA('Aditya')
+        assert ca.b == 0, "US102: default bal should be 0"
+    def test_ca_credit(self):
+        ca = bank.CA('Aditya')
+        ca.credit(100)
+        assert ca.b == 100, "US103: default bal should be 100"
+    def test_sa_credit(self):
+        sa = bank.SA('Aditya')
+        sa.credit(100)
+        assert sa.b == 1100, "US103: default bal should be 1100"
+
+unittest.main(verbosity=3)
+
