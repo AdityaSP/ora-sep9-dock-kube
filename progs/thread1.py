bde2500878c340a91077ef929a7ba4fa355d434e
Aditya <sp.aditya@gmail.com>
2019-09-14 01:29:41 +0530

    Add files via upload

--- progs/thread1.py
+++ progs/thread1.py
@@ -1,7 +1,9 @@
 import time
 from threading import Thread
+import threading
 
 def somework(x):
+    print(threading.current_thread().name)
     print("started", x)
     time.sleep(3)
     print("done", x)
@@ -9,10 +11,10 @@ def somework(x):
     
 #for i in range(5):
 #    somework(i)  #sync
-
+somework(100)
 
 for i in range(5):
-    t = Thread(target=somework, args=(i,))
+    t = Thread(target=somework, name='Thread-' + str(i), args=(i,))
     t.start()  #async
     
     
